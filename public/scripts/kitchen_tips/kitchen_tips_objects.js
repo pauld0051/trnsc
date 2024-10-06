f01b0cbd2859b87e68cc040b6f5ecc60d63b4eae
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2024-10-06 12:16:05 +0200

    Added mandolin slicer page

---
+++ public/scripts/kitchen_tips/kitchen_tips_objects.js
@@ -0,0 +1,11 @@
+const articles = [
+  {
+    title:
+      "Rediscovering Crispy Chips with a Mandolin Slicer: A Kitchen Essential",
+    description:
+      "Discover how a mandolin slicer transformed my kitchen, allowing for quick, easy, and healthy low-carb snacks. A must-have for anyone looking to enjoy crispy chips without the carbs!",
+    link: "/views/layouts/kitchen_tips/mandolin.html",
+    image: "/public/images/kitchen_tips/mandolin/sweet_potato_fries.jpg",
+    dateAdded: "2024-10-06",
+  },
+];
