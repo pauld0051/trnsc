8ff36e9f5b33cf37a9ae19bc0b64ca2c83a81968
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2024-10-13 10:04:50 +0200

    Added stories about stories

--- public/scripts/kitchen_tips/kitchen_tips_objects.js
+++ public/scripts/kitchen_tips/kitchen_tips_objects.js
@@ -8,4 +8,12 @@ const articles = [
     image: "/public/images/kitchen_tips/mandolin/sweet_potato_fries.jpg",
     dateAdded: "2024-10-06",
   },
+  {
+    title: "Why Recipe Sites Have Long Stories | The Real Naked Swedish Chef",
+    description:
+      "Frustrated by long stories before recipes? Learn why most recipe sites include them and how The Real Naked Swedish Chef is different—no ads, just real, honest food.",
+    link: "/views/layouts/kitchen_tips/stories.html",
+    image: "/public/images/kitchen_tips/stories/story_time2.jpeg",
+    dateAdded: "2024-10-13",
+  },
 ];
